92bc9c66599fdb35659fef6c2409f328485bfadf
Martino Facchin <m.facchin@arduino.cc>
2016-03-24 10:02:23 +0100

    port linux script to libusb 1.0.20 and dfu-util-0.9

--- compile_linux.sh
+++ compile_linux.sh
@@ -16,13 +16,13 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 mkdir -p distrib/linux64
-cd libusb-1.0.9
+cd libusb-1.0.20
 export LIBUSB_DIR=`pwd`
-./configure --enable-static --disable-shared
+./configure --enable-static --disable-shared --disable-udev
 make clean
-make -j4
+make
 cd ..
-cd dfu-util-0.8
+cd dfu-util-0.9
 USB_CFLAGS="-I$LIBUSB_DIR/libusb/" USB_LIBS="-L$LIBUSB_DIR/libusb/.libs/ -lusb-1.0 -lpthread" ./configure
 make clean
 CFLAGS=-static make
@@ -30,13 +30,13 @@ cp src/dfu-suffix src/dfu-prefix src/dfu-util  ../distrib/linux64/
 cd ..
 
 mkdir -p distrib/linux32
-cd libusb-1.0.9
+cd libusb-1.0.20
 export LIBUSB_DIR=`pwd`
-CFLAGS=-m32 ./configure
+CFLAGS=-m32 ./configure  --enable-static --disable-shared --disable-udev
 make clean
-make -j4
+make
 cd ..
-cd dfu-util-0.8
+cd dfu-util-0.9
 CFLAGS=-m32 USB_CFLAGS="-I$LIBUSB_DIR/libusb/" USB_LIBS="-L$LIBUSB_DIR/libusb/.libs/ -lusb-1.0 -lpthread" ./configure
 make clean
 CFLAGS=-static make
@@ -44,13 +44,13 @@ cp src/dfu-suffix src/dfu-prefix src/dfu-util ../distrib/linux32
 cd ..
 
 mkdir -p distrib/arm
-cd libusb-1.0.9
+cd libusb-1.0.20
 export LIBUSB_DIR=`pwd`
-./configure --enable-static --disable-shared --host=arm-linux-gnueabihf
+./configure --enable-static --disable-shared --disable-udev --host=arm-linux-gnueabihf
 make clean
-make -j4
+make
 cd ..
-cd dfu-util-0.8
+cd dfu-util-0.9
 USB_CFLAGS="-I$LIBUSB_DIR/libusb/" USB_LIBS="-L$LIBUSB_DIR/libusb/.libs/ -lusb-1.0 -lpthread" ./configure --host=arm-linux-gnueabihf
 make clean
 CFLAGS=-static make
