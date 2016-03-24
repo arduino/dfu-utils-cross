8e4dd5eb2eb7e574c4e9f030ec8d0d36c284442e
Martino Facchin <m.facchin@arduino.cc>
2016-03-24 14:19:37 +0100

    add no-ms-bitfields to CFLAGS

--- compile_win.sh
+++ compile_win.sh
@@ -19,12 +19,12 @@ export CFLAGS="-mno-ms-bitfields"
 mkdir -p distrib/windows
 cd libusb-1.0.20
 export LIBUSB_DIR=`pwd`
-./configure --host=i686-w64-mingw32 --enable-static --disable-shared
+CFLAGS="-mno-ms-bitfields -static" ./configure --host=i686-w64-mingw32 --enable-static --disable-shared
 make clean
-make
+CFLAGS="-mno-ms-bitfields -static" make
 cd ..
 cd dfu-util-0.9
-CFLAGS=-static USB_CFLAGS="-I$LIBUSB_DIR/libusb/" USB_LIBS="-L$LIBUSB_DIR/libusb/.libs/ -lusb-1.0" ./configure --host=i686-w64-mingw32
+CFLAGS="-mno-ms-bitfields -static" USB_CFLAGS="-I$LIBUSB_DIR/libusb/" USB_LIBS="-L$LIBUSB_DIR/libusb/.libs/ -lusb-1.0" ./configure --host=i686-w64-mingw32
 make clean
-CFLAGS=-static make
+CFLAGS="-mno-ms-bitfields -static" make
 cp src/*.exe ../distrib/windows/
