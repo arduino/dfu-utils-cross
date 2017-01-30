300d1e12993a1e29404ecf21642d147b9455894b
Arturo Rinaldi <arty.net2@gmail.com>
2017-01-30 22:55:52 +0100

    switching OSX sdk from darwin13 to darwin15

--- compile_mac.sh
+++ compile_mac.sh
@@ -18,12 +18,12 @@
 mkdir -p distrib/osx
 cd libusb-1.0.20
 export LIBUSB_DIR=`pwd`
-CC=o64-clang ./configure --host=x86_64-apple-darwin13 --enable-static --disable-shared
+CC=o64-clang ./configure --host=x86_64-apple-darwin15 --enable-static --disable-shared
 make clean
 make
 cd ..
 cd dfu-util-0.9
-CC=o64-clang USB_CFLAGS="-I$LIBUSB_DIR/libusb/ -framework IOKit -framework CoreFoundation" USB_LIBS="-L$LIBUSB_DIR/libusb/.libs/ -lusb-1.0" ./configure --host=x86_64-apple-darwin13
+CC=o64-clang USB_CFLAGS="-I$LIBUSB_DIR/libusb/ -framework IOKit -framework CoreFoundation" USB_LIBS="-L$LIBUSB_DIR/libusb/.libs/ -lusb-1.0" ./configure --host=x86_64-apple-darwin15
 make clean
 CFLAGS=-static make
 cp src/dfu-suffix src/dfu-prefix src/dfu-util ../distrib/osx/
