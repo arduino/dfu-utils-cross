362f5ba93f71057f6d53f0c0b0ad2f387f2ffe96
Arturo Rinaldi <arty.net2@gmail.com>
2017-01-31 10:36:41 +0100

    making OSX build platform independent

--- compile_mac.sh
+++ compile_mac.sh
@@ -15,15 +15,17 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
+platform=$(o64-clang -v 2>&1 | grep Target | awk {'print $2'} | sed 's/[.].*//g')
+
 mkdir -p distrib/osx
 cd libusb-1.0.20
 export LIBUSB_DIR=`pwd`
-CC=o64-clang ./configure --host=x86_64-apple-darwin15 --enable-static --disable-shared
+CC=o64-clang ./configure --host=$platform --enable-static --disable-shared
 make clean
 make
 cd ..
 cd dfu-util-0.9
-CC=o64-clang USB_CFLAGS="-I$LIBUSB_DIR/libusb/ -framework IOKit -framework CoreFoundation" USB_LIBS="-L$LIBUSB_DIR/libusb/.libs/ -lusb-1.0" ./configure --host=x86_64-apple-darwin15
+CC=o64-clang USB_CFLAGS="-I$LIBUSB_DIR/libusb/ -framework IOKit -framework CoreFoundation" USB_LIBS="-L$LIBUSB_DIR/libusb/.libs/ -lusb-1.0" ./configure --host=$platform
 make clean
 CFLAGS=-static make
 cp src/dfu-suffix src/dfu-prefix src/dfu-util ../distrib/osx/
